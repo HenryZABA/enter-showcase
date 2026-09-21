93695c20e5556c3feaeabf4db588232923fd516e
enter-pro <github@enter.pro>
2026-09-21 05:00:39 +0000

    Add vite-plugin-enter-dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,18 @@
+import { enterDevPlugin } from "vite-plugin-enter-dev";
+import { enterProdPlugin } from "vite-plugin-enter-dev";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
-export default defineConfig({ plugins: [react()], resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } }, server: { host: "127.0.0.1" } });
+export default defineConfig({
+  plugins: [react(), ...enterProdPlugin(), ...enterDevPlugin({
+    react: false
+  })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  server: {
+    host: "127.0.0.1"
+  }
+});
